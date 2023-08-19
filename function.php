c24589ec1a773963274c55f25c82f4291f342303
jokiww <jokoapriliyanto578@gmail.com>
2023-08-19 21:28:52 +0700

    don

--- function.php
+++ function.php
@@ -20,10 +20,11 @@ function registrasi($POST) {
     $username = strtolower($POST ["username"]);
     $password = mysqli_real_escape_string($db,$POST ["password"]);
     $password2 = mysqli_real_escape_string($db,$POST ["password2"]);
+    $level = mysqli_real_escape_string($db,$POST ["level"]);
 
     // cek username sudah ada atau belum
 
-    $result = mysqli_query($db, "SELECT username FROM user WHERE username = '$username'");
+    $result = mysqli_query($db, "SELECT username FROM users WHERE username = '$username'");
 
     if (mysqli_fetch_assoc($result)) {
         echo "<script>
@@ -42,7 +43,7 @@ function registrasi($POST) {
 
     // $password = password_hash($password,PASSWORD_DEFAULT);
     
-    mysqli_query($db,"INSERT INTO user values ('','$username','$password')");
+    mysqli_query($db,"INSERT INTO users values ('','$username','$password','$level')");
 
     return mysqli_affected_rows($db);
 }
--- function/flogin.php
+++ function/flogin.php
@@ -9,21 +9,21 @@ if (isset($_POST["login"])) {
 
 
 	// cek username ada atau tidak dalam databse
-	$result = mysqli_query($db, "SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+	$result = mysqli_query($db, "SELECT * FROM users WHERE username = '$username' AND password = '$password'");
 
 	// cek ada berapa baris username yang dikembalikan dari perintah SELECT
 	if (mysqli_num_rows($result) === 1) {
 		echo "
         <script language='JavaScript'>
         swal('Success!', 'Your data have been accepted. Thank you!', 'success').then(function(){ 
-                window.location.href = 'home1.html';
+                window.location.href = 'home1.php';
                }
             );
         </script>";
 	} else {
 		echo "<script>
     alert ('Data mengalami kegagalan! Harap ulang kembali');
-    document.location.href = 'home1.html';
+    document.location.href = 'home1.php';
     </script>
     ";
 
